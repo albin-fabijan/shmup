d9f22e5a7d9f3b93fc4a385ce3a8d0c04c52d502
theo-baccam <theo.baccam@laplateforme.io>
2024-04-09 13:46:08 +0200

    refactor(Bullet): dict for handling bullet directions instead of if conditions

--- source/Bullet.py
+++ source/Bullet.py
@@ -2,7 +2,7 @@ import pygame
 from .Paths import Paths
 
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, player, direction):
+    def __init__(self, player, current_direction):
         super(Bullet, self).__init__()    
         self.surf = pygame.image.load(Paths().select_sprite("bullet.png")).convert_alpha()
         self.size = self.surf.get_size()
@@ -10,18 +10,31 @@ class Bullet(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center=(player.rect.x+10, player.rect.y))
         self.speed = -20
         self.to_kill = False
-        self.direction = direction # 0 = up, 1 = down, 2 = right, 3 = left
+        self.directions = {
+            0: self._move_up,
+            1: self._move_down,
+            2: self._move_right,
+            3: self._move_left,
+        }
+        self.current_direction = current_direction
 
     def update(self, enemies):
-        if (self.direction == 0) :
-            self.rect.y += self.speed
-        elif (self.direction == 1) :
-            self.rect.y -= self.speed
-        elif (self.direction == 2) :
-            self.rect.x += self.speed
-        elif (self.direction == 3) :
-            self.rect.x -= self.speed
+        self.directions[self.current_direction]()
+
         if pygame.sprite.spritecollideany(self, enemies):
             self.to_kill = True
+
         if self.rect.bottom < -10:
             self.kill()
+
+    def _move_up(self):
+        self.rect.y += self.speed
+
+    def _move_down(self):
+        self.rect -= self.speed
+
+    def _move_right(self):
+        self.rect += self.speed
+
+    def _move_left(self):
+        self.rect -= self.speed
