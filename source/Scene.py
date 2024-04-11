0ec77ee50a6607fc0bde8c3e79a0ab37df5aa485
theo-baccam <theo.baccam@laplateforme.io>
2024-04-11 10:29:47 +0200

    docs(Scene): added docstrings for each method

--- source/Scene.py
+++ source/Scene.py
@@ -1,12 +1,21 @@
 class Scene:
     def initialization(self):
+        '''Called in the constructor, to create a scene's variables that
+        we can't really consider "attributes".
+        '''
         ...
 
     def update_internal_variables(self):
+        '''To update the internal variables of a scene such as entities,
+        rects, etc...
+        '''
         ...
 
     def event_loop(self):
+        '''Handling all pygame events such as user inputs'''
         ...
 
     def display(self):
+        '''Contains all display related functions such as screen-fills,
+        blitting, etc...'''
         ...
