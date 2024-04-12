3f82df2bd5c050a988b3bfd19dec6140c75e6e8f
theo-baccam <theo.baccam@laplateforme.io>
2024-04-12 13:54:01 +0200

    feat(GameOver): text for score
    may need to rework scoring system to prevent negative scores.
    for example using multiplication/division instead of substraction

--- source/GameOver.py
+++ source/GameOver.py
@@ -11,7 +11,6 @@ class GameOver(Scene):
         self.initialization()
 
     def initialization(self):
-        print(self.score)
         self.running = True
         self.background = pygame.image.load(
             Paths().select_sprite("background-menu.png")
@@ -25,10 +24,18 @@ class GameOver(Scene):
         self.back_button = pygame.Rect(
             (
                 self.window.SCREEN_WIDTH // 2 - (button_dimensions // 2),
-                self.window.SCREEN_HEIGHT // 2 - (button_dimensions // 2)
+                self.window.SCREEN_HEIGHT * 0.66 - (button_dimensions // 2)
             ),
             (200, 200)
         )
+
+        self.font = pygame.font.Font(None, 48)
+        self.score_text_surface = self.font.render(
+            str(self.score),
+            True,
+            (255, 255, 255)
+        )
+        self.score_text_middle = self.score_text_surface.get_width() // 2
 
     def update_internal_variables(self):
         ...
@@ -51,6 +58,14 @@ class GameOver(Scene):
             self.window.screen,
             (255, 0, 0),
             self.back_button
+        )
+
+        self.window.screen.blit(
+            self.score_text_surface,
+            (
+                (self.window.SCREEN_WIDTH // 2) - self.score_text_middle,
+                self.window.SCREEN_HEIGHT * 0.33
+            )
         )
 
     def next_scene(self):
