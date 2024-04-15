146b052967748b355683422ff598b13515998bf1
helwan-djibril-mimouni <163547268+helwan-djibril-mimouni@users.noreply.github.com>
2024-04-15 09:31:08 +0200

    refactor(levels_list.py): made levels shorter for maybe music

--- source/levels_list.py
+++ source/levels_list.py
@@ -1,32 +1,32 @@
 levels_list = [
     {
         "id": 1,
-        "boat_number": 4,
+        "boat_number": 2,
         "enemies": ["W", "W"],
     },
     {
         "id": 2,
-        "boat_number": 8,
-        "enemies": ["Y", "W", "W", "Y"],
+        "boat_number": 4,
+        "enemies": ["Y", "W", "W"],
     },
     {
         "id": 3,
-        "boat_number": 12,
-        "enemies": ["R", "Y", "W", "W", "Y", "R"],
+        "boat_number": 6,
+        "enemies": ["R", "Y", "W", "Y"],
     },
     {
         "id": 4,
-        "boat_number": 16,
-        "enemies": ["B", "R", "Y", "W", "W", "Y", "R", "B"],
+        "boat_number": 8,
+        "enemies": ["B", "R", "Y", "R", "B"],
     },
     {
         "id": 5,
-        "boat_number": 20,
-        "enemies": ["G", "B", "R", "Y", "W", "W", "Y", "R", "B", "G"],
+        "boat_number": 10,
+        "enemies": ["G", "B", "R", "Y", "W", "G"],
     },
     {
         "id": 6,
-        "boat_number": 24,
-        "enemies": ["V", "G", "B", "R", "Y", "W", "W", "Y", "R", "B", "G", "V"],
+        "boat_number": 12,
+        "enemies": ["V", "G", "B", "R", "Y", "G", "V"],
     },
 ]
